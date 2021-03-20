05b4e6586390bc8bf04403c5ce7c52bc469424cc
cheetahking123 <73245649+cheetahking123@users.noreply.github.com>
2021-03-21 03:47:00 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-        <h3>Contact information:</h3>
-        <p>Man Ho Wa&nbsp;&nbsp;~Student from Information Security</p>
-        <p>Email: 20017464d@connect.polyu.hk</p>
-        <p>&copy; Copyright  <?php echo date('Y')?> &nbsp;&nbsp;  &nbsp;&nbsp;All Right reserved.</p>
-    </footer>
-</body>
-</html>
