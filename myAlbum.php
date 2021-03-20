cf69bc04cfe51d68aa24512da4bc8a7ab31e8d1f
cheetahking123 <73245649+cheetahking123@users.noreply.github.com>
2021-03-21 03:46:11 +0800

    Delete myAlbum.php

--- myAlbum.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php');?>
-
-    <h1>My Album</h1>
-
-    <img src=/webhome/images/howard1 width=300 height=400>
-    <img src=/webhome/images/howard2 width=300 height=400>
-<?php include('footer.php');?>
